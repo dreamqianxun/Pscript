5bd9679406770a4e0da1f46c4667280c0fbfa08a
dreamqianxun <40326165+dreamqianxun@users.noreply.github.com>
2022-09-19 15:01:10 +0800

    Update Get.js

--- dx/Get.js
+++ dx/Get.js
@@ -8,6 +8,7 @@ hostname=czapp.bestpay.com.cn
 * */
 
 const $ = new Env('中国电信获取body');
+console.log($.read('Bodyswitch'))
 const effective = $request.body.indexOf("BILLCYCLE")
 let loginerror=$.read('Bodyswitch')
 
